19100472ced9cd89e046848b6433073103735f74
liam-b <liam@anywhich.com>
2019-04-13 12:06:51 +1000

    working on delays

--- Assets/Scenes/Game/Player/PlayerController.cs
+++ Assets/Scenes/Game/Player/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
   public BlockType selectedBlockType = BlockType.Cable;
   public int selectedRotation = BlockRotation.Right;
+  public int selectedLayer = 0;
+
+  public GridController grid;
 
   void Update() {
     if (Input.GetKeyDown(KeyCode.Alpha1)) selectedBlockType = BlockType.Cable;
@@ -13,5 +16,7 @@ public class PlayerController : MonoBehaviour {
     if (Input.GetKeyDown(KeyCode.Alpha4)) selectedBlockType = BlockType.Delay;
 
     if (Input.GetKeyDown(KeyCode.R)) selectedRotation = (selectedRotation + 1) % 4;
+    if (Input.GetKeyDown(KeyCode.LeftBracket)) selectedLayer = (selectedLayer - 1) % grid.layers;
+    if (Input.GetKeyDown(KeyCode.RightBracket)) selectedLayer = (selectedLayer + 1) % grid.layers;
   }
 }
