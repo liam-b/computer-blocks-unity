8fdaa7d0b432debfa4a35111841172e29a632074
liam-b <liam@anywhich.com>
2019-04-15 16:29:03 +1000

    added basic ui

---
+++ Assets/Scenes/Game/UI/LayerControllerUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerControllerUI : MonoBehaviour {
+  public GridController grid;
+  public RectTransform layerRect;
+  public int size = 100;
+
+  void Start() {
+    layerRect.sizeDelta = new Vector2(size, size / grid.layers);
+  }
+
+  void Update() {
+    layerRect.localPosition = new Vector2(size / 2, size / grid.layers / 2 + size / grid.layers * grid.player.selectedLayer);
+  }
+}
---
+++ Assets/Scenes/Game/UI/SelectedBlockControllerUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectedBlockControllerUI : MonoBehaviour {
+  public GridController grid;
+  public Image image;
+
+  public Sprite cableBlockSprite;
+  public Sprite sourceBlockSprite;
+  public Sprite inverterBlockSprite;
+  public Sprite delayBlockSprite;
+  public Sprite viaBlockSprite;
+
+  void Update() {
+    if (grid.player.selectedBlockType == BlockType.Cable) image.sprite = cableBlockSprite;
+    if (grid.player.selectedBlockType == BlockType.Source) image.sprite = sourceBlockSprite;
+    if (grid.player.selectedBlockType == BlockType.Inverter) image.sprite = inverterBlockSprite;
+    if (grid.player.selectedBlockType == BlockType.Delay) image.sprite = delayBlockSprite;
+    if (grid.player.selectedBlockType == BlockType.Via) image.sprite = viaBlockSprite;
+
+    image.rectTransform.eulerAngles = Vector3.forward * 90 * -grid.player.selectedRotation;
+  }
+}
